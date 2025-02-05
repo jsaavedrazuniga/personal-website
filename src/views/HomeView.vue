4ff339688b45019afccdc0b6b0365daddd37b179
Jheyson Saavedra <plump_bronzed.0z@icloud.com>
2025-02-05 10:47:35 -0500

    feat: setting the basic of the project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>This is the start of a great history.</h1>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
